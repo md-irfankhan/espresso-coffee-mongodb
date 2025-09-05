0ebfc0083f62c9986e25b87aa44a3c0c8d49c796
Md. Irfan Khan <learn.irfankhan@gmail.com>
2025-09-05 08:48:46 +0600

    coffee details page added

--- frontend/src/router/childrenRouters.jsx
+++ frontend/src/router/childrenRouters.jsx
@@ -1,9 +1,14 @@
+import Details from "../components/Details/Details";
 import Home from "../components/Home/Home";
 
 const childrenRouter=[
     {
         path:"/",
         element:<Home></Home>
+    },
+    {
+        path:'/details',
+        element:<Details></Details>
     }
 ]
 export default childrenRouter;
